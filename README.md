c8ab893fa49d80526d66f58f41b2cfdf25990086
Miguelrn <jmiguel.rnavarro@gmail.com>
2026-01-05 22:55:58 +0000

    raylib library linked to create game windows

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 gnatmake main.adb
+
+gnatmake -f main.adb -largs -l:libraylib.a -lm && ./main
--- main.adb
+++ main.adb
@@ -1,6 +1,25 @@
 with ADA.Text_IO; use ADA.Text_IO;
+with Interfaces.C; use Interfaces.C;
 
 procedure Main is 
+    procedure Init_Window(Width, Height : Int; Title : Char_Array) -- procedure return void
+        with Import => True, Convention => C, External_Name => "InitWindow";
+    function Window_Should_Close return int
+        with Import => True, Convention => C, External_Name => "WindowShouldClose";
+    procedure Begin_Drawing
+        with Import => True, Convention => C, External_Name => "BeginDrawing";
+    procedure Clear_Background
+        with Import => True, Convention => C, External_Name => "ClearBackground";
+    procedure Draw_Text
+        with Import => True, Convention => C, External_Name => "DrawText";
+    procedure End_Drawing 
+        with Import => True, Convention => C, External_Name => "EndDrawing";
 begin
-    Put_Line("Hello World");
+    Init_Window(800, 600, To_C("Hello from ADA")); -- Null termination handled by To_C
+    while Window_Should_Close = 0 loop
+        Begin_Drawing; 
+        --Clear_Background(RAYWHITE);
+        --Draw_Text("Hello, World!", 190, 200, 20, MAROON);
+        End_Drawing;
+    end loop;
 end;
